540d3eb76a983fda79b3fdbf384fb2aa7d0c55d5
Anthonius Munthi <me@itstoni.com>
2024-10-10 12:17:37 +0800

    feat(api): configured symfony framework, ref #60 :rocket:

---
+++ api/config/preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(dirname(__DIR__).'/var/cache/prod/Paroki_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/Paroki_KernelProdContainer.preload.php';
+}
