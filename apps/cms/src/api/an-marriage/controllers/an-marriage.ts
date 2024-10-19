9581ee07ff56cc970db7a72f3a906ad6f4aea767
Anthonius Munthi <me@itstoni.com>
2024-10-19 15:12:53 +0800

    feat(cms): removed cms from project :boom:

--- apps/cms/src/api/an-marriage/controllers/an-marriage.ts
+++
@@ -1,7 +0,0 @@
-/**
- * an-mariage controller
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController("api::an-marriage.an-marriage");
--- apps/cms/src/api/static/controllers/static.ts
+++
@@ -1,7 +0,0 @@
-/**
- * static controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::static.static');
