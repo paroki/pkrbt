44aa5e36536f23aff6469677b1ea6b590d2d89c7
Anthonius Munthi <me@itstoni.com>
2024-10-22 18:43:54 +0800

    refactor(cms): removed strapi from projects :boom:

--- apps/cms/src/api/an-marriage/routes/an-marriage.ts
+++
@@ -1,7 +0,0 @@
-/**
- * an-mariage router
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreRouter("api::an-marriage.an-marriage");
--- apps/cms/src/api/announcement/routes/announcement.ts
+++
@@ -1,9 +0,0 @@
-/**
- * announcement router
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreRouter("api::announcement.announcement", {
-  config: {},
-});
