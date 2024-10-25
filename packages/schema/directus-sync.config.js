ed6e4630b2f55d83c50915de6b4e7a7481b6eb98
Anthonius Munthi <me@itstoni.com>
2024-10-25 14:17:27 +0800

    feat(directus): added category sync schema :rocket:

--- packages/schema/directus-sync.config.js
+++ packages/schema/directus-sync.config.js
@@ -1,9 +1,21 @@
+const { createDirectus, authentication } = require("@directus/sdk");
 require("dotenv").config();
 
+const client = createDirectus("http://localhost:8055").with(
+  authentication("json"),
+);
+client
+  .login(process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD)
+  .then((result) => {
+    console.log(result);
+  });
+
 module.exports = {
+  debug: true,
   directusUrl: "http://localhost:8055",
-  directusEmail: process.env.ADMIN_EMAIL,
-  directusPassword: process.env.ADMIN_PASSWORD,
+  //directusEmail: process.env.ADMIN_EMAIL,
+  //directusPassword: process.env.ADMIN_PASSWORD,
+  /// directusToken: process.env.DIRECTUS_TOKEN,
   dumpPath: "./directus",
   // Additional options...
 };
