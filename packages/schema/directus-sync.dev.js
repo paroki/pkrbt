2a912ba358b450dca70f9703a340e1fc9124dc9c
Anthonius Munthi <me@itstoni.com>
2024-10-25 15:09:12 +0800

    feat(directus): moved docker compose to root project :rocket:

--- packages/schema/directus-sync.dev.js
+++ packages/schema/directus-sync.dev.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
+  debug: true,
   directusUrl: process.env.DIRECTUS_DEV_URL,
   directusToken: process.env.DIRECTUS_DEV_TOKEN,
   dumpPath: "./directus",
