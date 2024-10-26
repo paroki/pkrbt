8500ba6473550a683baa73324d9c402369418bfd
Anthonius Munthi <me@itstoni.com>
2024-10-26 15:56:08 +0800

    feat(directus): added registered users policy :rocket:

--- packages/schema/directus-sync.live.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  debug: true,
-  directusUrl: process.env.DIRECTUS_LIVE_URL,
-  directusToken: process.env.DIRECTUS_LIVE_TOKEN,
-  dumpPath: "./directus",
-};
