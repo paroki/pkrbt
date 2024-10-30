5576f8dfe0d37c2958f3f5b2e3f4aec520747d9c
Anthonius Munthi <me@itstoni.com>
2024-10-30 13:21:13 +0800

    fix(web): changed person unknown-person image

--- apps/web/src/app/dewan-pastoral-paroki/page.tsx
+++ apps/web/src/app/dewan-pastoral-paroki/page.tsx
@@ -1,38 +1,44 @@
-import Container from '@/components/ui/container';
-import { generateMeta } from '@/utils/meta';
-import { Metadata } from 'next';
-import { directus } from '@/utils/directus';
-import Image from 'next/image';
-import DirectusImage from '@/components/common/image';
-import { DM_Serif_Text } from 'next/font/google';
+import Container from "@/components/ui/container";
+import { generateMeta } from "@/utils/meta";
+import { Metadata } from "next";
+import { directus } from "@/utils/directus";
+import Image from "next/image";
+import DirectusImage from "@/components/common/image";
+import { DM_Serif_Text } from "next/font/google";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function generateMetadata(): Promise<Metadata> {
   return await generateMeta({
-    title: 'Dewan Pastoral Paroki',
-    description: 'Dewan Pastoral Paroki Paroki Kristus Raja Barong Tongkok, Keuskupan Agung Samarinda',
-    type: 'article'
+    title: "Dewan Pastoral Paroki",
+    description:
+      "Dewan Pastoral Paroki Paroki Kristus Raja Barong Tongkok, Keuskupan Agung Samarinda",
+    type: "article",
   });
 }
 
 const customFont = DM_Serif_Text({
-  subsets: ['latin'],
-  weight: '400'
+  subsets: ["latin"],
+  weight: "400",
 });
 export default async function Page() {
   const { items, error } = await directus.organisasi.jabatan.search({
     filter: {
       struktur: {
-        sort: 1100
-      }
+        sort: 1100,
+      },
     },
     fields: [
-      '*',
+      "*",
       {
-        penjabat: ['gelarDepan', 'nama', 'gelarBelakang', { foto: ['id', 'title', 'width', 'height', 'description'] }]
-      }
-    ]
+        penjabat: [
+          "gelarDepan",
+          "nama",
+          "gelarBelakang",
+          { foto: ["id", "title", "width", "height", "description"] },
+        ],
+      },
+    ],
   });
 
   if (!items) {
@@ -40,8 +46,14 @@ export default async function Page() {
   }
 
   if (error) {
-    throw new Error('Terjadi kesalahan', { cause: error });
+    throw new Error("Terjadi kesalahan", { cause: error });
   }
+
+  const noPhoto = {
+    url: "/static/unknown-person.jpg",
+    width: 481,
+    height: 640,
+  };
 
   return (
     <Container>
@@ -50,7 +62,9 @@ export default async function Page() {
           <span className="block font-bold text-lg lg:text-xl p-0 m-0 tracking-widest text-primary-600">
             PERIODE 2024-2027
           </span>
-          <span className={`${customFont.className} font-extrabold text-4xl md:text-5xl p-0 m-0`}>
+          <span
+            className={`${customFont.className} font-extrabold text-4xl md:text-5xl p-0 m-0`}
+          >
             Dewan Pastoral Paroki
           </span>
         </div>
@@ -59,27 +73,26 @@ export default async function Page() {
         </div>
         <div className="grid gap-4 items-start sm:grid-cols-2 md:grid-cols-3">
           {items.map((item, index) => (
-            <div key={index} className="flex flex-col justify-center items-center">
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center"
+            >
               {item?.penjabat.foto ? (
                 <DirectusImage
                   image={item.penjabat.foto}
                   style={{
-                    height: 300,
-                    width: 200,
-                    objectFit: 'cover'
+                    objectFit: "cover",
                   }}
                   className="rounded-lg overflow-hidden"
                 />
               ) : (
                 <Image
-                  src="/static/unknown-person.jpg"
-                  width={256}
-                  height={283}
+                  src={noPhoto.url}
+                  width={noPhoto.width}
+                  height={noPhoto.height}
                   alt={`foto dari ${item.nama}`}
                   style={{
-                    height: 300,
-                    width: 200,
-                    objectFit: 'cover'
+                    objectFit: "cover",
                   }}
                   className="rounded-lg"
                 />
@@ -87,9 +100,13 @@ export default async function Page() {
 
               <div className="my-2 flex flex-col items-center w-full text-center">
                 <span className="lg:text-xl font-bold text-primary-600">
-                  {item?.penjabat.gelarDepan ? `${item.penjabat.gelarDepan} ` : ''}
+                  {item?.penjabat.gelarDepan
+                    ? `${item.penjabat.gelarDepan} `
+                    : ""}
                   {item?.penjabat.nama}
-                  {item?.penjabat.gelarBelakang ? `, ${item.penjabat.gelarBelakang}` : ''}
+                  {item?.penjabat.gelarBelakang
+                    ? `, ${item.penjabat.gelarBelakang}`
+                    : ""}
                 </span>
                 <span className="lg:text-lg">{item.nama}</span>
               </div>
