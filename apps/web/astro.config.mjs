354573541ba58af5a745818292562aaa84c23d7d
Anthonius Munthi <me@itstoni.com>
2025-07-30 16:51:37 +0800

    fix(web): integrated vercel into astro build

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
+  output: "server",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
