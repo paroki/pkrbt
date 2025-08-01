1c67e4152959be580c31f78c2eca8557f1aeab7b
Anthonius Munthi <me@itstoni.com>
2025-08-01 18:32:57 +0800

    chore(ci): added base e2e ci testing

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,7 +3,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
