94e95127d013542c036f0371ebaae4ca629bd2bb
Anthonius Munthi <me@itstoni.com>
2025-08-02 15:59:29 +0800

    feat(db): integrated prisma into omed :rocket:

---
+++ packages/db/src/client.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "../generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
